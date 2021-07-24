38011776162d0a2ec4d5e910d9f40ec4be2a692f
Andrey Ivanov <andrey-tm203@mail.ru>
2021-07-24 22:50:05 +0300

    init jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  }
+};
