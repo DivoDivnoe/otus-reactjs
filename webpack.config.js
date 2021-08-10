67f5289f4695d7e24385c1f7fda3f2ed43048d18
Andrey Ivanov <andrey-tm203@mail.ru>
2021-08-10 22:59:09 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
