e25d9adbef2515c20be0dcefb0d7517533003156
Andrey Ivanov <andrey-tm203@mail.ru>
2021-08-22 14:46:08 +0300

    level 1 done

---
+++ src/homework3/level_1/2.ts
@@ -0,0 +1,45 @@
+// Задание первого уровня 2
+// Есть объединение (юнион) типов заказов в различных состояниях
+// Нужно заменить FIXME на тип который достанет из Order все возможные состояния (state)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FIXME =
+  | 'initial'
+  | 'inWork'
+  | 'buyingSupplies'
+  | 'producing'
+  | 'fullfilled';
+
+type Order =
+  | {
+      state: Extract<FIXME, 'initial'>;
+      sum: number;
+    }
+  | {
+      state: Extract<FIXME, 'inWork'>;
+      sum: number;
+      workerId: number;
+    }
+  | {
+      state: Extract<FIXME, 'buyingSupplies'>;
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+    }
+  | {
+      state: Extract<FIXME, 'producing'>;
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+    }
+  | {
+      state: Extract<FIXME, 'fullfilled'>;
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+      fullfillmentDate: Date;
+    };
+
+export const getOrderState = (order: Order): FIXME => order.state;
